68ab4ac5628d8578a31aa90898dd8d48429db0ff
Yasmin Reis Tello <mimirt08@gmail.com>
2022-05-25 19:24:11 -0300

    Requisito 07

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
  let pixelBoard= document.querySelector("#pixel-board")
+ 
 
   for(let index=0; index<5;index+=1){
       let linha= document.createElement('div')
@@ -11,6 +12,28 @@
     }
     
   }
+
+// consultei o stackoverflow para saber do classList.toggle 
+//Link: https://pt.stackoverflow.com/questions/146423/como-adicionar-e-remover-classes-com-javascript
+
+ let cor=document.querySelectorAll('.color')
+ let corSelected=document.querySelector('.selected')
+
+ for(index=0;index<cor.length;index+=1){
+
+ cor[index].addEventListener('click', function(event){
+  
+  for(index2=0; index2<cor.length;index2+=1){
+    cor[index2].classList.remove('selected')
+
+  }
+  event.target.classList.add('selected') 
+  })
+}
+
+
+
+
   
  
   
