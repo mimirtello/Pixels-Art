17804a2b1d85108a18c1cd9d3746f71734409c07
Yasmin Reis Tello <mimirt08@gmail.com>
2022-05-26 10:38:30 -0300

    Requisito 08

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 
  let pixelBoard= document.querySelector("#pixel-board")
- 
+ let pixels = document.getElementsByClassName("pixel")
 
   for(let index=0; index<5;index+=1){
       let linha= document.createElement('div')
       pixelBoard.appendChild(linha)
+      
     for(let index2=0; index2<5;index2+=1){
-      pixels=document.createElement('div')
-      pixels.className="pixel" 
-      linha.appendChild(pixels)
+      let pixel=document.createElement('div')
+      pixel.className="pixel" 
+      linha.appendChild(pixel)
     }
     
   }
+  
 
-// consultei o stackoverflow para saber do classList.toggle 
+// consultei o stackoverflow para saber do classList.add e remove
 //Link: https://pt.stackoverflow.com/questions/146423/como-adicionar-e-remover-classes-com-javascript
 
  let cor=document.querySelectorAll('.color')
- let corSelected=document.querySelector('.selected')
+ 
 
  for(index=0;index<cor.length;index+=1){
 
@@ -30,6 +32,23 @@
   event.target.classList.add('selected') 
   })
 }
+// Consultei documentação https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+//para usar o getComputedStyle
+console.log(pixels)
+
+for(let index3=0;index3<pixels.length;index3+=1){
+pixels[index3].addEventListener('click', function(event){
+  
+  let corSelected=document.getElementsByClassName('selected')[0]
+
+  let corSelecionada=window.getComputedStyle(corSelected).backgroundColor
+  pixels[index3].style.backgroundColor= corSelecionada
+  
+  })
+}
+    
+  
+
 
 
 
